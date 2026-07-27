923da9b48efc22fbe2e21032237a18d53d269c94
bobeau <mobeau.company@gmail.com>
2026-07-27 11:57:17 -0400

    Navigation vers la page Réparations

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -90,7 +90,7 @@ function Nav() {
         </Link>
 
         <nav className="hidden items-center gap-8 text-[13px] font-semibold text-ink md:flex">
-          <a href="#reparations" className="hover:text-racing">Réparations</a>
+          <Link to="/reparations" className="hover:text-racing">Réparations</Link>
           <Link to="/occasions" className="hover:text-racing">Occasions</Link>
           <Link to="/atelier" className="hover:text-racing">Atelier libre</Link>
           <a href="#contact" className="hover:text-racing">Contact</a>
